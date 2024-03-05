d4d3b5b80fc9cfe3b679da24123afe1edc2a3fe8
ssword <ssword@gmail.com>
2024-03-05 22:24:09 +0800

    added deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
+  adapter: netlify()
 });
